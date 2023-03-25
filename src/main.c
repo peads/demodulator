cb2ce23ae53db2b63065970229dddf3ada468a99
Patrick Eads <peads@users.noreply.github.com>
2023-03-24 21:44:53 -0400

    Loops (#2)
    * loop unrolling
    
    * loop unrolling
    
    * Revert "loop unrolling"
    
    This reverts commit 1df3afd3c76f8808545ddba5c71607f8ef5c210d.
    
    * Fixed error message
    
    * Incremental
    
    * Fixed parameters for offset tuning and rdc
    
    * Incremental
    
    * IT WORKS!
    
    * Cleaned up
    
    * Incremental
    
    * Incremental
    
    * Incremental
    
    * Incremental
    
    * Incremental
    
    * Incremental
    
    * Well, it works
    
    * Incremental
    
    * Incremental
    
    * Incremental

--- src/main.c
+++ src/main.c
@@ -21,13 +21,15 @@
 #include "demodulator.h"
 
 /**
- * Takes packed float representing the complex numbers
- * (ar + iaj), (br + ibj), s.t. z = {ar, aj, br, bj}
- * and returns their argument as a float
+ * Takes packed floats representing two sets of complex numbers
+ * of the form (ar + iaj), (br + ibj), s.t. z = {ar, aj, br, bj}
+ * and returns the arguments of the phasors (i.e. Arg[(ar + iaj).(br + ibj)])
+ * as a--effectively--a packed float. TODO doesn't account for special cases,
+ * but also doesn't seem to negatively affect demodulation, strangely. Should
+ * probably start handling those two cases again, eventually.
  **/
-extern float arg(__m128 z);
+extern uint64_t arg(__m128 a, __m128 b);
 __asm__(
-
 ".section:\n\t"
 ".p2align 4\n\t"
 "PI: "
@@ -39,101 +41,57 @@ __asm__(
 #else
 "arg: "
 #endif
-    "vpxor %xmm3, %xmm3, %xmm3\n\t"         // store zero
-    "vmulps _NEGATE_B_IM(%rip), %xmm0, %xmm0\n\t" // (ar, aj, br, -bj)
-    "vpermilps $0xEB, %xmm0, %xmm1\n\t"     // (ar, aj, br, bj) => (aj, aj, ar, ar)
-    "vpermilps $0x5, %xmm0, %xmm0\n\t"      // and                 (bj, br, br, bj)
+//    "vpxor %ymm3, %ymm3, %ymm3\n\t"         // store zero
 
-    "vmulps %xmm1, %xmm0, %xmm0\n\t"        // aj*bj, aj*br, ar*br, ar*bj
-    "vpermilps $0x8D, %xmm0, %xmm2\n\t"     // aj*br, aj*bj, ar*bj, ar*br
-    "vaddsubps %xmm2, %xmm0, %xmm0\n\t"     //  ... [don't care], ar*bj + aj*br, ar*br - aj*bj, [don't care] ...
-    "vmulps %xmm0, %xmm0, %xmm1\n\t"        // ... , (ar*bj + aj*br)^2, (ar*br - aj*bj)^2, ...
-    "vpermilps $0x1B, %xmm1, %xmm2\n\t"
-    "vaddps %xmm2, %xmm1, %xmm1\n\t"        // ..., (ar*br - aj*bj)^2 + (ar*bj + aj*br)^2, ...
+    "vblendps $0b0011, %xmm1, %xmm0, %xmm1\n\t"
+    "vinsertf128 $1, %xmm1, %ymm0, %ymm0\n\t"
+    "vmulps _NEGATE_B_IM(%rip), %ymm0, %ymm0\n\t" // (ar, aj, br, -bj)
 
-    "vpermilps $0x01, %xmm0, %xmm2\n\t"
-    "vcomiss %xmm2, %xmm3\n\t"
-    "jnz showtime\n\t"
-    "vpermilps $0x02, %xmm0, %xmm2\n\t"
-    "vcomiss %xmm3, %xmm2\n\t"
-    "jz zero\n\t"
-    "ja showtime\n\t"
-    "vmovq PI(%rip), %xmm0\n\t"
-    "ret \n\t"
+    "vpermilps $0xEB, %ymm0, %ymm1\n\t"     // (ar, aj, br, bj) => (aj, aj, ar, ar)
+    "vpermilps $0x5, %ymm0, %ymm0\n\t"      // and                 (bj, br, br, bj)
 
-"zero: "
-    "vmovq %xmm3,%xmm0\n\t"
-    "ret\n\t"
+    "vmulps %ymm1, %ymm0, %ymm0\n\t"        // aj*bj, aj*br, ar*br, ar*bj
+    "vpermilps $0x8D, %ymm0, %ymm2\n\t"     // aj*br, aj*bj, ar*bj, ar*br
+    "vaddsubps %ymm2, %ymm0, %ymm0\n\t"     //  ... [don't care], ar*bj + aj*br, ar*br - aj*bj, [don't care] ...
+    "vmulps %ymm0, %ymm0, %ymm1\n\t"        // ... , (ar*bj + aj*br)^2, (ar*br - aj*bj)^2, ...
+    "vpermilps $0x1B, %ymm1, %ymm2\n\t"
+    "vaddps %ymm2, %ymm1, %ymm1\n\t"        // ..., (ar*br - aj*bj)^2 + (ar*bj + aj*br)^2, ...
 
-"showtime: "                                // approximating atan2 with atan(z)
-                                            //   = z/(1 + (9/32) z^2) for z = (64 y)/(23 x + 41 Sqrt[x^2 + y^2])
-    "vrsqrtps %xmm1, %xmm1\n\t"             // ..., 1/Sqrt[(ar*br - aj*bj)^2 + (ar*bj + aj*br)^2], ...
-    "vmulps %xmm1, %xmm0, %xmm0\n\t"        // ... , zj/||z|| , zr/||z|| = (ar*br - aj*bj) / Sqrt[(ar*br - aj*bj)^2 + (ar*bj + aj*br)^2], ...
+    // TODO Add handling for the negative, Real half-plane (i.e., x<0, y==0 case)
+    // or is the overhead worth the "improvement"?
 
-    "vmulps _ALL_64S(%rip), %xmm0, %xmm2\n\t"        // 64*zj
-    "vmulps _ALL_23S(%rip), %xmm0, %xmm3\n\t"        // 23*zr
-    "vaddps _ALL_41S(%rip), %xmm3, %xmm3\n\t"        // 23*zr + 41
-    "vpermilps $0x1B, %xmm3, %xmm3\n\t"
-    "vrcpps %xmm3, %xmm3\n\t"
-    "vmulps %xmm3, %xmm2, %xmm0\n\t"
+    "vrsqrtps %ymm1, %ymm1\n\t"             // ..., 1/Sqrt[(ar*br - aj*bj)^2 + (ar*bj + aj*br)^2], ...
+    "vmulps %ymm1, %ymm0, %ymm0\n\t"        // ... , zj/||z|| , zr/||z|| = (ar*br - aj*bj) / Sqrt[(ar*br - aj*bj)^2 + (ar*bj + aj*br)^2], ...
 
-    "vpermilps $0x01, %xmm0, %xmm0\n\t"
+    "vmulps _ALL_64S(%rip), %ymm0, %ymm2\n\t"        // 64*zj
+    "vmulps _ALL_23S(%rip), %ymm0, %ymm3\n\t"        // 23*zr
+    "vaddps _ALL_41S(%rip), %ymm3, %ymm3\n\t"        // 23*zr + 41
+    "vpermilps $0x1B, %ymm3, %ymm3\n\t"
+    "vrcpps %ymm3, %ymm3\n\t"
+    "vmulps %ymm3, %ymm2, %ymm0\n\t"
+
+    "vextractf128 $1, %ymm0, %xmm1\n\t"
+    "vpermilps $1, %ymm0, %ymm0\n\t"
+    "vblendps $1, %xmm0, %xmm1, %xmm0\n\t"
+    "vcmpps $0x0, %xmm0, %xmm0, %xmm1\n\t"  // NAN check
+    "vandps %xmm1, %xmm0, %xmm0\n\t"
+    "vmovq %xmm0, %rax\n\t"
     "ret\n\t"
 );
 
-extern __m128 applySquelch(__m128 z, __m128 squelch);
-__asm__(
-#ifdef __clang__
-"_applySquelch: "
-#else
-"applySquelch: "
-#endif
-    "vmulps %xmm0, %xmm0, %xmm2\n\t"
-    "vpermilps $0xB1, %xmm2, %xmm3\n\t"
-    "vaddps %xmm2, %xmm3, %xmm2\n\t"
-    "vmulps _ALL_HUNDREDTHS(%rip), %xmm2, %xmm2\n\t"
-    "vcmpps $0x1D, %xmm1, %xmm2, %xmm2\n\t"
-    "vandps %xmm2, %xmm0, %xmm0\n\t"
-    "ret"
-);
+//static inline struct rotationMatrix generateRotationMatrix(const float theta, const float phi) {
+//
+//    const float cosT = cosf(theta);
+//    const float sinP = sinf(phi);
+//
+//    struct rotationMatrix result = {
+//        .a1 = {cosT, -sinP, cosT, -sinP},
+//        .a2 = {sinP, cosT, sinP, cosT}
+//    };
+//
+//    return result;
+//}
 
-/**
- * Takes a 4x4 matrix and applies it to a 4x1 vector.
- * Here, it is used to apply the same rotation matrix to
- * two complex numbers. i.e., for the the matrix
- * T = {{a,b}, {c,d}} and two vectors {u1,u2} and {v1,v2}
- * concatenated, s.t. u = {u1,u2,v1,v2}, Tu =
- * {a*u1 + c*u1, b*u2 + d*u2, ... , b*v2 + d*v2}
- */
-extern __m128 apply4x4_4x1Transform(const struct rotationMatrix *T, __m128 u);
-__asm__(
-#ifdef __clang__
-"_apply4x4_4x1Transform: "
-#else
-"apply4x4_4x1Transform: "
-#endif
-    "vmulps 16(%rdi), %xmm0, %xmm2\n\t"      // u1*a11, u2*a12, u3*a13, ...
-    "vmulps (%rdi), %xmm0, %xmm1\n\t"        // u1*a21, u2*a22, ...
-    "vpermilps $0xB1, %xmm2, %xmm0\n\t"
-    "vaddps %xmm2, %xmm0, %xmm2\n\t"         // u1*a11 + u2*a12, ... , u3*a13 + u4*a14
-    "vpermilps $0xB1, %xmm1, %xmm0\n\t"
-    "vaddps %xmm1, %xmm0, %xmm1\n\t"         // u1*a21 + u2*a22, ... , u3*a23 + u4*a24
-    "vblendps $0xA, %xmm2, %xmm1, %xmm0\n\t" // u1*a11 + u2*a12, u1*a21 + u2*a22,
-    "ret"                                    // u3*a13 + u4*a14, u3*a23 + u4*a24
-);
-
-static inline struct rotationMatrix generateRotationMatrix(const float theta, const float phi) {
-
-    const float cosT = cosf(theta);
-    const float sinP = sinf(phi);
-
-    struct rotationMatrix result = {
-        .a1 = {cosT, -sinP, cosT, -sinP},
-        .a2 = {sinP, cosT, sinP, cosT}
-    };
-
-    return result;
-}
 extern uint64_t filter(__m128 *buf, uint64_t len, uint8_t downsample);
 __asm__(
 #ifdef __clang__
@@ -165,57 +123,124 @@ __asm__(
     "ret"
 );
 
-static void removeDCSpike(__m128 *buf, const uint32_t len) {
+extern void removeDCSpike(__m128 *buf, uint64_t len);
+__asm__(
+#ifdef __clang__
+"_removeDCSpike: "
+#else
+"removeDCSpike: "
+#endif
+    "movq %rdi, %rcx\n\t"   // store array address
+    "movq %rsi, %rax\n\t"   // store n
+    "shlq $4, %rax\n\t"
+    "addq %rax, %rcx\n\t"   // store address of end of array
+    "negq %rax\n\t"
+    "vmovaps _dc_avg_iq(%rip), %xmm1\n\t"
+"L3: "
+    "vmovaps (%rcx,%rax), %xmm0\n\t"
+    "vsubps %xmm1, %xmm0, %xmm1\n\t"
+    "vmulps _DC_RAW_CONST(%rip), %xmm1, %xmm1\n\t"
+    "vaddps %xmm1, %xmm1, %xmm1\n\t"
+    "vsubps %xmm1, %xmm0, %xmm0\n\t"
+    "vmovaps %xmm0, (%rcx,%rax)\n\t"
+    // loop unroll one
+    "vmovaps 16(%rcx,%rax), %xmm0\n\t"
+    "vsubps %xmm1, %xmm0, %xmm1\n\t"
+    "vmulps _DC_RAW_CONST(%rip), %xmm1, %xmm1\n\t"
+    "vaddps %xmm1, %xmm1, %xmm1\n\t"
+    "vsubps %xmm1, %xmm0, %xmm0\n\t"
+    "vmovaps %xmm0, 16(%rcx,%rax)\n\t"
+    // loop unroll two
+    "vmovaps 32(%rcx,%rax), %xmm0\n\t"
+    "vsubps %xmm1, %xmm0, %xmm1\n\t"
+    "vmulps _DC_RAW_CONST(%rip), %xmm1, %xmm1\n\t"
+    "vaddps %xmm1, %xmm1, %xmm1\n\t"
+    "vsubps %xmm1, %xmm0, %xmm0\n\t"
+    "vmovaps %xmm0, 32(%rcx,%rax)\n\t"
+    // loop unroll three
+    "vmovaps 48(%rcx,%rax), %xmm0\n\t"
+    "vsubps %xmm1, %xmm0, %xmm1\n\t"
+    "vmulps _DC_RAW_CONST(%rip), %xmm1, %xmm1\n\t"
+    "vaddps %xmm1, %xmm1, %xmm1\n\t"
+    "vsubps %xmm1, %xmm0, %xmm0\n\t"
+    "vmovaps %xmm0, 48(%rcx,%rax)\n\t"
+    // i += 4
+    "addq $64, %rax\n\t"
+    "jl L3\n\t"
+    "vmovaps %xmm1, _dc_avg_iq(%rip)\n\t"
+    "ret"
+);
 
-    static __m128 dcAvgIq = {0, 0, 0, 0};
-    uint64_t i;
+extern void rotateForNonOffsetTuning(__m128 *buf, uint64_t len);
+__asm__(
+#ifdef __clang__
+"_rotateForNonOffsetTuning: "
+#else
+"rotateForNonOffsetTuning: "
+#endif
+    "movq %rdi, %rcx\n\t"   // store array address
+    "movq %rsi, %rax\n\t"    // store n
+    "shlq $4, %rax\n\t"
+    "addq %rax, %rcx\n\t"    // store address of end of array
+    "negq %rax\n\t"
+"L5: "
+    "vmovaps (%rcx,%rax), %xmm0\n\t"
+    "vmulps _CNJ_TRANSFORM(%rip), %xmm0, %xmm0\n\t"
+    "vmovaps %xmm0, (%rcx,%rax)\n\t"
+    "addq $16, %rax\n\t"
+    "jl L5\n\t"
+    "ret"
+);
 
-    for (i = 0; i < len; ++i) {
-        dcAvgIq = _mm_add_ps(dcAvgIq, _mm_mul_ps(DC_RAW_CONST, _mm_sub_ps(buf[i], dcAvgIq)));
-        buf[i] = _mm_sub_ps(buf[i], dcAvgIq);
-    }
-}
+extern uint64_t demodulateFmData(__m128 *buf, uint64_t len, uint64_t *result);
+__asm__(
+#ifdef __clang__
+"_demodulateFmData: "
+#else
+"demodulateFmData: "
+#endif
+    "movq %rdi, %rcx\n\t"   // store array address
+    "movq %rsi, %r8\n\t"    // store n
+    "shlq $4, %r8\n\t"
+    "addq %r8, %rcx\n\t"    // store address of end of array
 
-static void rotateForNonOffsetTuning(__m128 *buf, const uint32_t len) {
+    "movq %rdx, %r9\n\t"    // store result address
+    "shrq %r8\n\t"
+    "addq %r8, %r9\n\t"     // store address of end of result
+    "shlq %r8\n\t"
 
-    uint64_t i;
-
-    for (i = 0; i < len; ++i) {
-        buf[i] = apply4x4_4x1Transform(&CONJ_TRANSFORM, buf[i]);
-    }
-}
-
-static uint64_t demodulateFmData(__m128 *buf, const uint32_t len, float **result) {
-
-    uint64_t i, j;
-
-    *result = calloc(len << 1, OUTPUT_ELEMENT_BYTES);
-    for (i = 0, j = 0; i < len; ++i, j += 2) {
-        (*result)[j] = arg(buf[i]);
-        (*result)[j + 1] = arg(_mm_blend_ps(buf[i], buf[i + 1], 0b0011));
-    }
-
-    return j;
-}
+    "negq %r8\n\t"
+"L4: "
+    "vmovaps (%rcx,%r8), %xmm1\n\t"
+    "vmovaps -16(%rcx,%r8), %xmm0\n\t"
+    "call _arg\n\t"
+    "sarq %r8\n\t"
+    "movq %rax, (%r9,%r8)\n\t"
+    "shlq %r8\n\t"
+    "addq $16, %r8\n\t"
+    "jl L4\n\t"
+    "shlq $1, %rsi\n\t"
+    "movq %rsi, %rax\n\t"
+    "ret"
+);
 
 static void checkFileStatus(FILE *file) {
 
     if (ferror(file)) {
         char errorMsg[256];
-        sprintf(errorMsg, "I/O error when reading file");
+        sprintf(errorMsg, "\nI/O error when reading file");
         perror(errorMsg);
         exitFlag = 1;
     } else if (feof(file)) {
-        fprintf(stderr, "Exiting\n");
+        fprintf(stderr, "\nExiting\n");
         exitFlag = EOF;
     }
 }
 
-static void *processMatrix(void *ctx) {
-
-    struct readArgs *args = ctx;
+static void *processMatrix(struct readArgs *args) {
+    
     uint64_t depth;
-    float *result;
+    uint64_t *result;
 
     if (args->isRdc) {
         removeDCSpike(args->buf, args->len);
@@ -226,7 +251,8 @@ static void *processMatrix(void *ctx) {
     }
 
     depth = filter(args->buf, args->len, args->downsample);
-    depth = demodulateFmData(args->buf, depth, &result);
+    result = calloc(depth << 1, OUTPUT_ELEMENT_BYTES);
+    depth = demodulateFmData(args->buf, depth, result);
 
 
     fwrite(result, OUTPUT_ELEMENT_BYTES, depth, args->outFile);
@@ -259,12 +285,16 @@ static int readFileData(struct readArgs *args) {
             "vpmovsxbw %0, %0\n\t"
             "vpmovsxwd %0, %0\n\t"
             "vcvtdq2ps %0, %0\n\t"
-        :"=x"(args->buf[j]):"x"(z.v));
-
-        if (args->squelch) {
-            args->buf[j] = applySquelch(args->buf[j], *args->squelch);
-        }
-        j++;
+            "orq %2, %2\n\t"
+            "jz nosquelch\n\t"
+            "vmulps %0, %0, %%xmm2\n\t"
+            "vpermilps $0xB1, %%xmm2, %%xmm3\n\t"
+            "vaddps %%xmm2, %%xmm3, %%xmm2\n\t"
+            "vmulps _ALL_HUNDREDTHS(%%rip), %%xmm2, %%xmm2\n\t"
+            "vcmpps $0x1D, (%2), %%xmm2, %%xmm2\n\t"
+            "vandps %%xmm2, %0, %0\n\t"
+        "nosquelch: "
+        :"=x"(args->buf[j++]):"x"(z.v),"r"(args->squelch):"xmm2","xmm3");
 
         if (!exitFlag && j >= args->len) {
             args->len = j;
@@ -276,9 +306,6 @@ static int readFileData(struct readArgs *args) {
     fclose(inFile);
     fclose(args->outFile);
     free(args->buf);
-    if (args->squelch) {
-        free(args->squelch);
-    }
 
     return exitFlag;
 }
@@ -288,25 +315,26 @@ int main(int argc, char **argv) {
     static struct readArgs args;
     int opt;
     int exitCode;
+    __m128 squelch;
 
     if (argc < 3) {
         return -1;
     } else {
-        while ((opt = getopt(argc, argv, "r:i:o:d:f:s:")) != -1) {
+        while ((opt = getopt(argc, argv, "i:o:d:s:rf")) != -1) {
             switch (opt) {
                 case 'r':
-                    args.isRdc = atoi(optarg);
+                    args.isRdc = 1;
                     break;
                 case 'f':
-                    args.isOt = atoi(optarg);
+                    args.isOt = 1;
                     break;
                 case 'd':
                     args.downsample = atoi(optarg);
                     break;
                 case 's':   // TODO add parameter to take into account the impedance of the system
                     // currently calculated for 50 Ohms (i.e. Prms = ((I^2 + Q^2)/2)/50 = (I^2 + Q^2)/100)
-                    args.squelch = malloc(MATRIX_ELEMENT_BYTES);
-                    *args.squelch = _mm_set1_ps(powf(10.f, atof(optarg) / 10.f));
+                    squelch = _mm_set1_ps(powf(10.f, (float) atof(optarg) / 10.f));
+                    args.squelch = &squelch;
                     break;
                 case 'i':
                     if (!strstr(optarg, "-")) {
@@ -329,6 +357,6 @@ int main(int argc, char **argv) {
     }
 
     exitCode = readFileData(&args) != EOF ? 1 : 0;
-    fprintf(stderr, "%s\n", exitCode ? "Exiting with error" : "Exiting");
+    fprintf(stderr, "%s\n", exitCode ? "Exited with error" : "Exited");
     return exitCode;
 }
