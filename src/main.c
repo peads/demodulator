58bb280bba5a44b9d1c2e01472d505546112bd2e
Patrick Eads <peads@users.noreply.github.com>
2023-03-30 14:40:57 -0400

    Stuff (#12)
    * Apparently the SV  ABI does not specify any xmm registers as non-clobbers.

--- src/main.c
+++ src/main.c
@@ -19,32 +19,31 @@
  */
 #include <stdio.h>
 #include <stdint.h>
-#include <immintrin.h>
 #include <string.h>
 #include <math.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 struct chars {
     uint8_t isRdc;      // 0
     uint8_t isOt;       // 1
-    uint8_t downsample; // 2
+    //uint8_t downsample; // 2
 };
 
-extern int8_t processMatrix(__m128 squelch, FILE *inFile, struct chars *chars, FILE *outFile);
+extern int8_t processMatrix(float squelch, FILE *inFile, struct chars *chars, FILE *outFile);
 
 int main(int argc, char **argv) {
 
     int opt;
-    __m128 squelch = {0,0,0,0};
+    float temp = 0.f;
     FILE *inFile = NULL;
     FILE *outFile = NULL;
-
     struct chars chars;
 
     if (argc < 3) {
         return -1;
     } else {
-        while ((opt = getopt(argc, argv, "i:o:d:s:rf")) != -1) {
+        while ((opt = getopt(argc, argv, "i:o:s:rf")) != -1) {
             switch (opt) {
                 case 'r':
                     chars.isRdc = 1;
@@ -52,12 +51,12 @@ int main(int argc, char **argv) {
                 case 'f':
                     chars.isOt = 1;
                     break;
-                case 'd':
-                    chars.downsample = atoi(optarg);
-                    break;
+//                case 'd': // TODO reimplement downsmapling?
+//                    chars.downsample = atoi(optarg);
+//                    break;
                 case 's':   // TODO add parameter to take into account the impedance of the system
                             // currently calculated for 50 Ohms (i.e. Prms = ((I^2 + Q^2)/2)/50 = (I^2 + Q^2)/100)
-                    squelch = _mm_set1_ps(powf(10.f, (float) atof(optarg) / 10.f));
+                    temp = powf(10.f, (float) atof(optarg) / 10.f);
                     break;
                 case 'i':
                     if (!strstr(optarg, "-")) {
@@ -80,5 +79,5 @@ int main(int argc, char **argv) {
             }
         }
     }
-    return processMatrix(squelch, inFile, &chars, outFile) != EOF;
+    return processMatrix(temp, inFile, &chars, outFile) != EOF;
 }
