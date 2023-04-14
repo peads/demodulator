32193f717a19b1a38ecc28eec040562c659516a8
Patrick Eads <peads@users.noreply.github.com>
2023-04-14 17:10:13 -0400

    Fixing write osx (#20)
    Fixed output the output in the darwin abi by replacing calls to printf with syscalls to write.

---
+++ include/asm.h
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the demodulator distribution
+ * (https://github.com/peads/demodulator).
+ * with code originally part of the misc_snippets distribution
+ * (https://github.com/peads/misc_snippets).
+ * Copyright (c) 2023 Patrick Eads.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef DEMODULATOR_ASM_H
+#define DEMODULATOR_ASM_H
+
+#include "definitions.h"
+
+#ifdef __APPLE__
+#define FREAD _fread
+#define STATUS _checkFileStatus
+#define FERROR _ferror
+#define FEOF _feof
+#define PERROR _perror
+//DARWIN x64 ONLY
+#define SYS_WRITE 0x2000004
+#define SYS_OPEN 0x2000005
+#define SYS_CLOSE 0x2000006
+#define O_WRONLY 0x1
+#define O_CREAT 0x200
+#define O_TRUNC 0x400
+    .globl  _processMatrix
+    _processMatrix:
+#else
+#define SYS_WRITE 0x1
+#define SYS_OPEN 0x2
+#define SYS_CLOSE 0x3
+#define O_WRONLY 0x1
+#define O_CREAT 0100
+#define O_TRUNC 01000
+#define FREAD fread
+#define STATUS checkFileStatus
+#define FERROR ferror
+#define FEOF feof
+#define PERROR perror
+    .globl  processMatrix
+    processMatrix:
+#endif
+#endif //DEMODULATOR_ASM_H
--- src/main.c
+++ src/main.c
@@ -24,14 +24,15 @@
 #include <stdlib.h>
 #include "matrix.h"
 
-extern int8_t processMatrix(float squelch, FILE *inFile, struct chars *chars, FILE *outFile);
+extern int processMatrix(float squelch, FILE *inFile, struct chars *chars, char *outFile);
 
 int main(int argc, char **argv) {
 
+    int ret = 1;
     int opt;
     float temp = 0.f;
     FILE *inFile = NULL;
-    FILE *outFile = NULL;
+    char *outFile = NULL;
     struct chars chars;
     chars.isOt = 0;
     chars.isRdc = 0;
@@ -47,9 +48,6 @@ int main(int argc, char **argv) {
                 case 'f':
                     chars.isOt = 1;
                     break;
-//                case 'd': // TODO reimplement downsmapling?
-//                    chars.downsample = atoi(optarg);
-//                    break;
                 case 's':   // TODO add parameter to take into account the impedance of the system
                             // currently calculated for 50 Ohms (i.e. Prms = ((I^2 + Q^2)/2)/50 = (I^2 + Q^2)/100)
                     temp = powf(10.f, (float) atof(optarg) / 10.f);
@@ -63,20 +61,15 @@ int main(int argc, char **argv) {
                     }
                     break;
                 case 'o':
-                    if (!strstr(optarg, "-")) {
-                        outFile = fopen(optarg, "wb");
-                    } else {
-                        freopen(NULL, "wb", stdout);
-                        outFile = stdout;
-                    }
+                    outFile = !strstr(optarg, "-") ? optarg : NULL;
                     break;
                 default:
                     break;
             }
         }
     }
-    int ret = processMatrix(temp, inFile, &chars, outFile) != EOF;
+    ret = processMatrix(temp, inFile, &chars, outFile);
     fclose(inFile);
-    fclose(outFile);
+//    fclose(outFile);
     return ret;
 }
