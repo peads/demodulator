cff669e37d2313e26b5dfdb75adf079f9e17e70f
Patrick Eads <peads@users.noreply.github.com>
2023-08-22 22:49:44 -0400

    Added info and usage example. (#24)

---
+++ example.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sox -twav SDRSharp_20160101_231914Z_12kHz_IQ.wav -traw -b8 -eunsigned-integer -r512k - | build/demodulator -i - -o - | sox -v10 -traw -ef -r512k -b32 - -traw -b16 -es -r48k - | dsd -i - -o /dev/null
