8d461f29f0f573993991a82fc6a2caee1451c8d1
Patrick Eads <peads@users.noreply.github.com>
2023-08-24 08:25:52 -0400

    Added build script because I am lazy, and make example script nicer

---
+++ cmake_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CUST_OPTS=""
+INSTALL_PREFIX="/usr/local"
+
+if [ ! -z $1 ]; then
+    CUST_OPTS=$1
+fi
+
+if [ ! -z $2 ]; then
+    INSTALL_PREFIX=$2
+fi
+
+echo $INSTALL_PREFIX
+echo $CUST_OPTS
+
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release $CUST_OPTS -S . -B build
+cmake --build build
+cmake --install build --prefix $INSTALL_PREFIX
