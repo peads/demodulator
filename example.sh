43f338e6384494051a44bfca3c63cb326c88a84b
Patrick Eads <peads@users.noreply.github.com>
2023-08-24 08:26:44 -0400

    Added build script because I am lazy, and make example script nicer (#26)

--- example.sh
+++ example.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sox -twav SDRSharp_20160101_231914Z_12kHz_IQ.wav -traw -b8 -eunsigned-integer -r512k - | build/demodulator -i - -o - | sox -traw -ef -r512k -b32 - -traw -b16 -es -r48k - | dsd -i - -o /dev/null
+sox -D -twav SDRSharp_20160101_231914Z_12kHz_IQ.wav -traw -b8 -eunsigned-integer -r512k - | build/demodulator -i - -o - | sox -D -v1.25 -traw -ef -r512k -b32 - -traw -b16 -es -r48k - | dsd -i - -o /dev/null
+
