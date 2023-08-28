a02054d5fd06f741cd4d3b339253523107c7b121
Patrick Eads <peads@users.noreply.github.com>
2023-08-28 18:58:09 -0400

    incremental

--- include/matrix.h
+++ include/matrix.h
@@ -6,16 +6,18 @@
 #define DEMODULATOR_MATRIX_H
 #include <stdint.h>
 
+typedef void (*conversionFunction_t)(const void *__restrict__, const uint32_t, float *__restrict__);
+typedef float (*fastRsqrtFun_t)(float);
+
 struct chars {
     uint8_t isRdc;      // 0
     uint8_t isOt;       // 1
     //uint8_t downsample; // 2
 };
 
-union fastSqrtPun {
+union fastRsqrtPun {
     uint32_t i;
     float f;
 };
-
 
 #endif //DEMODULATOR_MATRIX_H
--- src/matrix.c
+++ src/matrix.c
@@ -23,28 +23,75 @@
 #include "definitions.h"
 #include "matrix.h"
 
-void fmDemod(const uint8_t *__restrict__ buf, const uint32_t len, float *__restrict__ result) {
+void noconversion(const void *__restrict__ in, const uint32_t index,
+                  float *__restrict__ out)  {
+
+    const float *buf = (float *) in;
+
+    out[0] = (buf[index] + buf[index + 2]);
+    out[1] = (buf[index + 1] + buf[index + 3]);
+    out[2] = (buf[index + 4] + buf[index + 6]);
+    out[3] = -(buf[index + 5] + buf[index + 7]);
+}
+
+void convertInt16ToFloat(const void *__restrict__ in, const uint32_t index,
+                         float *__restrict__ out) {
+
+    const int16_t *buf = (int16_t *) in;
+
+    out[0] = (float) (buf[index] + buf[index + 2]);
+    out[1] = (float) (buf[index + 1] + buf[index + 3]);
+    out[2] = (float) (buf[index + 4] + buf[index + 6]);
+    out[3] = (float) -(buf[index + 5] + buf[index + 7]);
+}
+
+void convertUint8ToFloat(const void *__restrict__ in, const uint32_t index,
+                         float *__restrict__ out) {
+
+    const uint8_t *buf = (uint8_t *) in;
+
+    out[0] = (float) (buf[index] + buf[index + 2] - 254);       // ar
+    out[1] = (float) (254 - buf[index + 1] - buf[index + 3]);   // aj
+    out[2] = (float) (buf[index + 4] + buf[index + 6] - 254);   // br
+    out[3] = (float) (buf[index + 5] + buf[index + 7] - 254);   // bj
+}
+
+float fastRsqrt(float y) {
+
+    static union fastRsqrtPun pun;
+
+    pun.f = y;
+    pun.i = -(pun.i >> 1) + 0x5f3759df;
+    pun.f *= 0.5f * (-y * pun.f * pun.f + 3.f);
+
+    return pun.f;
+}
+
+float slowRsqrt(float y) {
+    return 1.f/sqrtf(y);
+}
+
+void fmDemod(const void *__restrict__ buf, const uint32_t len, float *__restrict__ result,
+             conversionFunction_t convert, fastRsqrtFun_t rsqrt) {
+
+    static float out[4] = {0.f, 0.f, 0.f, 0.f};
+    static float *ar = out;
+    static float *aj = out + 1;
+    static float *br = out + 2;
+    static float *bj = out + 3;
 
     uint32_t i;
-    float ar, aj, br, bj, zr, zj, y;
+    float zr, zj, y;
 
     for (i = 0; i < len; i++) {
 
-        ar = (float) (buf[i] + buf[i + 2] - 254);
-        aj = (float) (254 - buf[i + 1] - buf[i + 3]);
+        convert(buf, i, out);
 
-        br = (float) (buf[i + 4] + buf[i + 6] - 254);
-        bj = (float) (buf[i + 5] + buf[i + 7] - 254);
+        zr = fmaf(*ar, *br, -*aj * *bj);
+        zj = fmaf(*ar, *bj, *aj * *br);
+        y = rsqrt(fmaf(zr, zr, zj * zj));
 
-        zr = fmaf(ar, br, -aj * bj);
-        zj = fmaf(ar, bj, aj * br);
-
-        union fastSqrtPun pun = {.f = fmaf(zr, zr, zj * zj)};
-        y = -pun.f;
-        pun.i = -(pun.i >> 1) + 0x5f3759df;
-        pun.f *= 0.5f * (3.f + y * pun.f * pun.f);
-
-        zr = 64.f * zj * pun.f * 1.f / fmaf(zr * pun.f, 23.f, 41.f);
+        zr = 64.f * zj * y * 1.f / fmaf(zr * y, 23.f, 41.f);
 
         result[i >> 2] = isnan(zr) ? 0.f : zr;
     }
@@ -52,14 +99,16 @@ void fmDemod(const uint8_t *__restrict__ buf, const uint32_t len, float *__restr
 
 int processMatrix(float squelch, FILE *inFile, struct chars *chars, void *outFile) {
 
-    uint8_t *buf = calloc(DEFAULT_BUF_SIZE, INPUT_ELEMENT_BYTES);
+    const size_t shiftedSize = DEFAULT_BUF_SIZE - 2;
+
+    void *buf = calloc(DEFAULT_BUF_SIZE, INPUT_ELEMENT_BYTES);
     int exitFlag = 0;
     size_t readBytes;
     float result[QTR_BUF_SIZE];
 
     while (!exitFlag) {
 
-        readBytes = fread(buf + 2, INPUT_ELEMENT_BYTES, DEFAULT_BUF_SIZE - 2, inFile);
+        readBytes = fread(buf + 2, INPUT_ELEMENT_BYTES, shiftedSize, inFile);
 
         if ((exitFlag = ferror(inFile))) {
             perror(NULL);
@@ -68,9 +117,9 @@ int processMatrix(float squelch, FILE *inFile, struct chars *chars, void *outFil
             exitFlag = EOF;
         }
 
-        fmDemod(buf, readBytes, result);
+        fmDemod(buf, readBytes, result, convertUint8ToFloat, fastRsqrt);
 
-        fwrite(result, OUTPUT_ELEMENT_BYTES, QTR_BUF_SIZE, outFile);
+        fwrite(result, OUTPUT_ELEMENT_BYTES, readBytes >> 2, outFile);
     }
     return exitFlag;
 }
