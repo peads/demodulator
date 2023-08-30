60814d2a66ca3173046517bbd810c8853d755b02
Patrick Eads <peads@users.noreply.github.com>
2023-08-30 20:43:20 +0100

    incremental

--- include/matrix.h
+++ include/matrix.h
@@ -17,21 +17,13 @@
 #endif
 
 #if defined(HAS_AVX) || defined(HAS_SSE)
-    #define HAS_EITHER
+    #define HAS_EITHER_AVX_OR_SSE
+#endif
+
+#if defined(__aarch64__) || defined(_M_ARM64)
+    #define HAS_AARCH64
 #endif
 
 typedef void (*conversionFunction_t)(const void *__restrict__, const uint32_t, float *__restrict__);
-typedef float (*fastRsqrtFun_t)(float);
-
-struct chars {
-    uint8_t isRdc;      // 0
-    uint8_t isOt;       // 1
-    //uint8_t downsample; // 2
-};
-
-union fastRsqrtPun {
-    uint32_t i;
-    float f;
-};
 
 #endif //DEMODULATOR_MATRIX_H
