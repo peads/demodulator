03beebfa04090a5ed3b9c0714132022ecd5b6ddf
Patrick Eads <peads@users.noreply.github.com>
2023-09-10 13:41:46 -0400

    incremental

--- include/prototypes.h
+++ include/prototypes.h
@@ -6,5 +6,5 @@
 #define DEMODULATOR_PROTOTYPES_H
 #include <stdint.h>
 #include <stdio.h>
-int processMatrix(FILE *__restrict__ inFile, uint8_t mode, float gain, void *__restrict__ outFile);
+int processMatrix(FILE *__restrict__ inFile, uint8_t mode, float inGain, void *__restrict__ outFile);
 #endif //DEMODULATOR_PROTOTYPES_H
--- intrinsics/intel/new_matrix.c
+++ intrinsics/intel/new_matrix.c
@@ -37,8 +37,8 @@ typedef union {
 } pun256Int;
 
 typedef union {
-    __m256 v;
-    float arr[8];
+    __m256 *v;
+    float *arr;
 } pun256f32;
 
 typedef union {
@@ -100,7 +100,7 @@ static inline __m256 gather(__m128 u, __m128 v) {
     return _mm256_mul_ps(_mm256_set_m128(_mm_blend_ps(u, v, 0b0011), u), negateBIm);
 }
 
-static inline void preMultNorm(__m256 *u, __m256 *v) {
+static inline void preNormMult(__m256 *u, __m256 *v) {
 
     *v = _mm256_permute_ps(*u, 0xEB);   //  {bj, br, br, bj, bj, br, br, bj} *
                                         //  {aj, aj, ar, ar, cj, cj, cr, cr}
@@ -108,15 +108,20 @@ static inline void preMultNorm(__m256 *u, __m256 *v) {
     *u = _mm256_mul_ps(_mm256_permute_ps(*u, 0x5),*v);
 }
 
-static inline void foo(__m256 *u, __m256 *v, __m256 *w) {
+static inline void preNormAddSubAdd(__m256 *u, __m256 *v, __m256 *w) {
 
     *w = _mm256_permute_ps(*u, 0x8D);         // {aj, bj, ar, br, cj, dj, cr, dr}
     *u = _mm256_addsub_ps(*u, *w);     // {ar-aj, aj+bj, br-ar, bj+br, cr-cj, cj+dj, dr-cr, dj+dr}
-    *v = _mm256_mul_ps(*u,
-            *u);        // {(ar-aj)^2, (aj+bj)^2, (br-ar)^2, (bj+br)^2, (cr-cj)^2, (cj+dj)^2, (dr-cr)^2, (dj+dr)^2}
+    *v = _mm256_mul_ps(*u,*u);         // {(ar-aj)^2, (aj+bj)^2, (br-ar)^2, (bj+br)^2, (cr-cj)^2, (cj+dj)^2, (dr-cr)^2, (dj+dr)^2}
     *w = _mm256_permute_ps(*v, 0x1B);        // {ar^2, aj^2, br^2, bj^2, cr^2, cj^2, dr^2, dj^2} +
     // {bj^2, br^2, aj^2, ar^2, ... }
     *v = _mm256_add_ps(*v, *w);       // = {ar^2+bj^2, aj^2+br^2, br^2+aj^2, bj^2+ar^2, ... }
+}
+
+static inline void norm(__m256 *u, __m256 *v) {
+
+    *v = _mm256_rsqrt_ps(*v);
+    *u = _mm256_mul_ps(*u, *v);
 }
 
 static float fmDemod(__m128 x) {
@@ -127,25 +132,24 @@ static float fmDemod(__m128 x) {
 
     static __m128 prev = {0.f, 0.f, 0.f, 0.f};
 
-    pun256f32 ret;
     __m128 u0 = prev;
     __m128 v0 = prev = x;
     __m256 v, w, y;
     __m256 u = gather(u0, v0); // {ar, aj, br, bj, cr, cj, br, bj}
+    pun256f32 ret = {&u};
 
-    preMultNorm(&u, &v);
-    foo(&u, &v, &w);
+    preNormMult(&u, &v);
+    preNormAddSubAdd(&u, &v, &w);
+    norm(&u, &v);
 
-    v = _mm256_rsqrt_ps(v);
-    u = _mm256_mul_ps(u, v);
-
-    w = _mm256_mul_ps(u, all64s);                  // 64*zj
-    u = _mm256_fmadd_ps(all23s, u, all41s);     // 23*zr + 41s
+    // fast atan2 -> atan2(x,y) = 64y/(23x+41)
+//    w = _mm256_mul_ps(u, all64s);                     // 64*zj
+    u = _mm256_fmadd_ps(all23s, u, all41s);   // 23*zr + 41
     y = _mm256_rcp_ps(_mm256_permute_ps(u, 0x1B));
-    u = _mm256_mul_ps(w, y);
+    u = _mm256_mul_ps(_mm256_mul_ps(u, all64s), y);
 
-    v = _mm256_cmp_ps(u, u, 0);                           // NAN check
-    ret.v = _mm256_and_ps(u, v);
+    v = _mm256_cmp_ps(u, u, 0);                          // NAN check
+    u = _mm256_and_ps(u, v);
 
     return ret.arr[5];
 }
