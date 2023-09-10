404cd870f7c8a4ba90dc2cfafec2997742e0c476
Patrick Eads <peads@users.noreply.github.com>
2023-09-10 16:41:00 -0400

    actually done done

--- example.sh
+++ example.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-sox -D -twav SDRSharp_20160101_231914Z_12kHz_IQ.wav -traw -eunsigned-int -b8 -r512k - \
-  | build/demodulator -r1 -i - -o -| sox -traw -b32 -ef -r128k - -traw -es -b16 -r48k - \
-  | dsd -i - -o /dev/null -n
-sox -D -twav SDRSharp_20160101_231914Z_12kHz_IQ.wav -traw -es -b16 -r512k -  \
-  | build/demodulator -i - -o - | sox -traw -b32 -ef -r128k - -traw -es -b16 -r48k - \
-  | dsd -i - -o /dev/null -n
+./cmake_build.sh -DIS_INTINT=ON &&  sox -D -twav SDRSharp_20160101_231914Z_12kHz_IQ.wav -traw -eunsigned-int -b8 -r512k - | build/demodulator -i - -o - -r1 | sox -traw -b32 -ef -r128k - -traw -es -b16 -r48k - | dsd -i - -o /dev/null -n
+
+sox -D -twav SDRSharp_20160101_231914Z_12kHz_IQ.wav -traw -es -b16 -r512k - | build/demodulator -i - -o - | sox -traw -b32 -ef -r128k - -traw -es -b16 -r48k - | dsd -i - -o /dev/null -n
+
+time build/demodulator -i uint8.dat -o file -r1
+time build/demodulator -i uint8.dat -o file -r1
+time build/demodulator -i uint8.dat -o file -r1
+sox -traw -b32 -ef -r128k file -traw -es -b16 -r48k - | dsd -i - -o /dev/null -n
+rm file
+time build/demodulator -i int16.dat -o file
+time build/demodulator -i int16.dat -o file
+time build/demodulator -i int16.dat -o file
+sox -traw -b32 -ef -r128k file -traw -es -b16 -r48k - | dsd -i - -o /dev/null -n
--- intrinsics/intel/matrix.c
+++ intrinsics/intel/matrix.c
@@ -18,64 +18,111 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 #include <stdio.h>
-#include <math.h>
+
+#ifdef __GNUC__
+
+#include <stdint.h>
+
+#endif
+
 #include <immintrin.h>
+#include <math.h>
 #include "definitions.h"
 #include "matrix.h"
 
-typedef __m128 (*mm_convert_fun_t)(__m128i u);
+typedef __m256i (*vectorOp256_t)(__m256i);
+typedef __m128 (*vectorOp128_t)(__m128i);
 
-typedef union {
-    uint8_t arr[16];
-    __m128i v;
-} pun128u8;
-
-typedef union {
-    __m256 v;
-    float arr[8];
-} pun256f32;
+typedef struct {
+    vectorOp256_t boxcar;
+    vectorOp256_t conj;
+    vectorOp256_t convertIn;
+    vectorOp128_t convertOut;
+} vectorOps_t;
 
 static __m128 convertInt16ToFloat(__m128i u) {
 
-    return _mm_cvtepi32_ps(
-            _mm_cvtepi16_epi32(u));
+    return _mm_cvtepi32_ps(_mm_cvtepi16_epi32(u));
 }
 
-static __m128 convertUint8ToFloat(__m128i u) {
+static __m256i convertUint8ToInt8(__m256i u) {
 
-    static const __m128i Z = {-0x7f7f7f7f7f7f7f7f, -0x7f7f7f7f7f7f7f7f};
-
-    return convertInt16ToFloat(_mm_cvtepi8_epi16(_mm_add_epi16(u, Z)));
+    static const __m256i Z = {-0x7f7f7f7f7f7f7f7f, -0x7f7f7f7f7f7f7f7f, -0x7f7f7f7f7f7f7f7f, -0x7f7f7f7f7f7f7f7f};
+    return _mm256_add_epi8(u, Z);
 }
 
-static inline __m128 conju(__m128 u) {
+static __m128 convertInt8ToFloat(__m128i u) {
 
-    static const __m128 Z = {1.f, -1.f, 1.f, -1.f};
-    return _mm_mul_ps(u, Z);
+    return convertInt16ToFloat(_mm_cvtepi8_epi16(u));
 }
 
-static inline __m128 boxcar(__m128 u) {
+static __m256i conjUint8(__m256i u) {
 
-    return _mm_add_ps(u, _mm_permute_ps(u, 0x4E));
+    static const __m256i mask = {
+            0x0606040402020000, 0x0e0e0c0c0a0a0808,
+            0x0606040402020000, 0x0e0e0c0c0a0a0808};
+    static const __m256i mask2 = {
+            (int64_t) 0x8000800080008000, (int64_t) 0x8000800080008000,
+            (int64_t) 0x8000800080008000, (int64_t) 0x8000800080008000};
+    static const __m256i Z = {
+            (int64_t) 0xff01ff01ff01ff01,
+            (int64_t) 0xff01ff01ff01ff01,
+            (int64_t) 0xff01ff01ff01ff01,
+            (int64_t) 0xff01ff01ff01ff01};
+
+    __m256i v = u;
+    __asm__(
+            "VPMULHW %0, %1, %0\n\t"
+            : "+x" (v): "x" (Z)
+            );
+
+    return _mm256_blendv_epi8(u, _mm256_shuffle_epi8(v, mask), mask2);
+}
+
+static __m256i boxcarUint8(__m256i u) {
+
+    static const __m256i mask = {
+            0x0504070601000302, 0x0d0c0f0e09080b0a,
+            0x0504070601000302, 0x0d0c0f0e09080b0a};
+    return _mm256_add_epi8(u, _mm256_shuffle_epi8(u, mask));
+}
+
+static __m256i boxcarInt16(__m256i u) {
+
+    return _mm256_add_epi16(u, _mm256_shufflelo_epi16(u, 0x4E));;
+}
+
+static __m256i conjInt16(__m256i u) {
+
+    static const __m256i Z = {
+            (int64_t) 0xffff0001ffff0001,
+            (int64_t) 0xffff0001ffff0001,
+            (int64_t) 0xffff0001ffff0001,
+            (int64_t) 0xffff0001ffff0001};
+
+    return _mm256_mullo_epi16(Z, u);
 }
 
 static inline __m256 gather(__m128 u, __m128 v) {
 
-    static const __m256 negateBIm = {1.f, 1.f, 1.f, -1.f, 1.f, 1.f, 1.f, -1.f};
+    static const __m256 negateBIm = {1.f, 1.f, 1.f, -1.f, 1.f, 1.f, 1.f, -1.f}; //0x010101FF...
 
     return _mm256_mul_ps(_mm256_set_m128(_mm_blend_ps(u, v, 0b0011), u), negateBIm);
 }
 
 static inline void preNormMult(__m256 *u, __m256 *v) {
-    *v = _mm256_permute_ps(*u, 0xEB);                         //  {bj, br, br, bj, bj, br, br, bj} *
-//    u = _mm256_permute_ps(u, 0x5);                        //  {aj, aj, ar, ar, cj, cj, cr, cr}
-    *u = _mm256_mul_ps(_mm256_permute_ps(*u, 0x5), *v);  // = {aj*bj, aj*br, ar*br, ar*bj, bj*cj, br*cj, br*cr, bj*cr}
+
+    *v = _mm256_permute_ps(*u, 0xEB);   //  {bj, br, br, bj, bj, br, br, bj} *
+                                        //  {aj, aj, ar, ar, cj, cj, cr, cr}
+                                        // = {aj*bj, aj*br, ar*br, ar*bj, bj*cj, br*cj, br*cr, bj*cr}
+    *u = _mm256_mul_ps(_mm256_permute_ps(*u, 0x5),*v);
 }
 
 static inline void preNormAddSubAdd(__m256 *u, __m256 *v, __m256 *w) {
+
     *w = _mm256_permute_ps(*u, 0x8D);         // {aj, bj, ar, br, cj, dj, cr, dr}
     *u = _mm256_addsub_ps(*u, *w);     // {ar-aj, aj+bj, br-ar, bj+br, cr-cj, cj+dj, dr-cr, dj+dr}
-    *v = _mm256_mul_ps(*u, *u);        // {(ar-aj)^2, (aj+bj)^2, (br-ar)^2, (bj+br)^2, (cr-cj)^2, (cj+dj)^2, (dr-cr)^2, (dj+dr)^2}
+    *v = _mm256_mul_ps(*u,*u);         // {(ar-aj)^2, (aj+bj)^2, (br-ar)^2, (bj+br)^2, (cr-cj)^2, (cj+dj)^2, (dr-cr)^2, (dj+dr)^2}
     *w = _mm256_permute_ps(*v, 0x1B);        // {ar^2, aj^2, br^2, bj^2, cr^2, cj^2, dr^2, dj^2} +
                                              // {bj^2, br^2, aj^2, ar^2, ... }
     *v = _mm256_add_ps(*v, *w);       // = {ar^2+bj^2, aj^2+br^2, br^2+aj^2, bj^2+ar^2, ... }
@@ -83,24 +130,25 @@ static inline void preNormAddSubAdd(__m256 *u, __m256 *v, __m256 *w) {
 
 static float fmDemod(__m128 x) {
 
-    static __m128 prev = {0.f, 0.f, 0.f, 0.f};
-
     static const __m256 all64s = {64.f, 64.f, 64.f, 64.f, 64.f, 64.f, 64.f, 64.f};
     static const __m256 all23s = {23.f, 23.f, 23.f, 23.f, 23.f, 23.f, 23.f, 23.f};
     static const __m256 all41s = {41.f, 41.f, 41.f, 41.f, 41.f, 41.f, 41.f, 41.f};
 
-    pun256f32 ret;
+    static __m128 prev = {0.f, 0.f, 0.f, 0.f};
+
     __m128 u0 = prev;
     __m128 v0 = prev = x;
-    __m256 u, v, w, y;
+    __m256 v, w, y;
+    __m256 u = gather(u0, v0); // {ar, aj, br, bj, cr, cj, br, bj}
 
-    u = gather(u0, v0);
     preNormMult(&u, &v);
     preNormAddSubAdd(&u, &v, &w);
 
+    // Norm
     v = _mm256_rsqrt_ps(v);
     u = _mm256_mul_ps(u, v);
 
+    // fast atan2 -> atan2(x,y) = 64y/(23x+41)
     w = _mm256_mul_ps(u, all64s);                  // 64*zj
     u = _mm256_fmadd_ps(all23s, u, all41s);     // 23*zr + 41s
     y = _mm256_rcp_ps(_mm256_permute_ps(u, 0x1B));
@@ -109,56 +157,85 @@ static float fmDemod(__m128 x) {
     v = _mm256_cmp_ps(u, u, 0);                           // NAN check
     u = _mm256_and_ps(u, v);
 
-    ret.v = u;
-    return ret.arr[5];
+    return u[5];
 }
 
-static inline mm_convert_fun_t processMode(const uint8_t mode) {
+static __m256i nonconversion(__m256i u) {
+    return u;
+}
+
+static inline int processMode(const uint8_t mode, vectorOps_t *funs) {
 
     switch (mode) {
         case 0: // default mode (input int16)
-            return convertInt16ToFloat;
+            funs->boxcar = boxcarInt16;
+            funs->conj = conjInt16;
+            funs->convertIn = nonconversion;
+            funs->convertOut = convertInt16ToFloat;
+            break;
         case 1: // input uint8
-            return convertUint8ToFloat;
+            funs->boxcar = boxcarUint8;
+            funs->conj = conjUint8;
+            funs->convertIn = convertUint8ToInt8;
+            funs->convertOut = convertInt8ToFloat;
+            break;
         default:
-            return NULL;
+            return -1;
     }
+    return 0;
 }
 
-int processMatrix(FILE *__restrict__ inFile,
-                  uint8_t mode,
-                  float gain,
+int processMatrix(FILE *__restrict__ inFile, uint8_t mode, const float inGain,
                   void *__restrict__ outFile) {
 
-    static pun128u8 x;
-    static float result[DEFAULT_BUF_SIZE >> 2];
+    vectorOps_t *funs = malloc(sizeof(*funs));
+    int exitFlag = processMode(mode, funs);
+    void *buf = NULL;
+    float result[MATRIX_WIDTH];
+
+    exitFlag += posix_memalign(&buf, 16, MATRIX_WIDTH << 3);
+    exitFlag += posix_memalign((void **) &result, 16, MATRIX_ELEMENT_BYTES);
+
+    size_t readBytes = 0;
+    __m128i lo, hi;
+    __m256i v;
 
     const size_t inputElementBytes = 2 - mode;
-    const uint8_t isGain = fabsf(1.f - gain) > GAIN_THRESHOLD;
-    const mm_convert_fun_t convert = processMode(mode);
+    const uint8_t isGain = fabsf(1.f - inGain) > GAIN_THRESHOLD;
+    const __m128 gain = _mm_broadcast_ss(&inGain);
 
-    int exitFlag = mode < 0 || mode > 2;
-    size_t readBytes, i;
-    float ret;
-    // TODO change/add pre-demodulation gain, s.t. we leverage simd
     while (!exitFlag) {
-        for (i = 0, readBytes = 0; i < DEFAULT_BUF_SIZE; i += OUTPUT_ELEMENT_BYTES) {
 
-            readBytes += fread(x.arr, inputElementBytes, OUTPUT_ELEMENT_BYTES, inFile);
+        readBytes += fread(buf, inputElementBytes, MATRIX_WIDTH << 3, inFile);
 
-            if ((exitFlag = ferror(inFile))) {
-                perror(NULL);
-                break;
-            } else if (feof(inFile)) {
-                exitFlag = EOF;
-            }
-
-            ret = fmDemod(boxcar(conju(convert(x.v))));
-            result[i >> 2] = isGain ? ret * gain : ret;
+        if ((exitFlag = ferror(inFile))) {
+            perror(NULL);
+            break;
+        } else if (feof(inFile)) {
+            exitFlag = EOF;
         }
 
-        fwrite(result, OUTPUT_ELEMENT_BYTES, readBytes >> 2, outFile);
+        v = funs->boxcar(funs->conj(funs->convertIn(*(__m256i *) buf)));
+        lo = _mm256_castsi256_si128(v);
+        hi = _mm256_extracti128_si256(v, 1);
+
+        result[0] = fmDemod(funs->convertOut(_mm_movpi64_epi64(*(__m64 *) (&lo))));
+        result[1] = fmDemod(funs->convertOut(_mm_movpi64_epi64(
+                (__m64) _mm_extract_epi64(lo,1))));
+        result[2] = fmDemod(funs->convertOut(_mm_movpi64_epi64(*(__m64 *) (&hi))));
+        result[3] = fmDemod(funs->convertOut(_mm_movpi64_epi64(
+                (__m64) _mm_extract_epi64(hi,1))));
+
+        if (isGain) {
+            _mm_mul_ps(*(__m128 *)&result, gain);
+        }
+
+        fwrite(result, OUTPUT_ELEMENT_BYTES, MATRIX_WIDTH, outFile);
     }
 
+    free(buf);
+    free(funs);
+
+    printf("Total bytes read: %lu\n", readBytes);
     return exitFlag;
 }
