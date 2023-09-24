eb892b7c6c673d7afa20223a90cc3718e3874157
Patrick Eads <peads@users.noreply.github.com>
2023-09-24 13:38:35 -0400

    New example sh (#40)
    * init
    
    * done

--- example.sh
+++ example.sh
@@ -1,47 +1,75 @@
-#!/bin/bash
-
-arr=()
-
-type gcc >/dev/null 2>&1
-if [ "$?" == 0 ]; then
-  echo ":: COMPILER INFO"
-  echo "`gcc --version`"
-  arr[${#arr[@]}]=$(which gcc)
-  echo ":: END COMPILER INFO"
+audioOutOpts="-o/dev/null -n"
+if [ ! -z "$1" ]; then
+    audioOutOpts="-o${1}"
 fi
 
-type clang >/dev/null 2>&1
-if [ "$?" == 0 ]; then
-  echo ":: COMPILER INFO"
-  echo "`clang --version`"
-  arr[${#arr[@]}]=$(which clang)
-  echo ":: END COMPILER INFO"
-fi
+declare -A opts=(
+  ["-DIS_INTRINSICS=ON"]="128k"
+  ["-DIS_INTRINSICS=ON -DNO_AVX512=ON"]="128k"
+  ["-DIS_INTRINSICS=OFF"]="256k"
+)
+declare -A arr
 
-type icc >/dev/null 2>&1
-if [ "$?" == 0 ]; then
-  echo ":: COMPILER INFO"
-  echo "`icc -diag-disable=10441 --version`"
-  arr[${#arr[@]}]=$(which icc)
-  echo ":: END COMPILER INFO"
-fi
+function gen() {
+
+  type $1 >/dev/null 2>&1
+
+  if [ "$?" == 0 ]; then
+
+    echo ":: COMPILER INFO"
+    echo "$(${1} --version)"
+    echo ":: END COMPILER INFO"
+    key="-DCMAKE_C_COMPILER=$(which ${1})"
+    for val in "${!opts[@]}"; do
+      arr[$key $val]=${opts[$val]}
+    done
+  fi
+}
+
+function printRunInfo() {
+
+  echo ":: RUN INFO"
+  echo ":: $2"
+  echo ":: COMPILER OPTIONS=$1"
+  echo ":: END RUN INFO"
+}
+
+gen gcc
+gen clang
+gen icc
 
 set -e
 
-rm -rf file int16.dat uint8.dat ||:
-for compiler in ${arr[@]}; do
+for key in "${!arr[@]}"; do
 
-  ./cmake_build.sh "-DIS_VERBOSE=ON -DIS_INTRINSICS=ON -DCMAKE_C_COMPILER=$compiler"
+  compiler=`sh -c "./cmake_build.sh \"${key}\" | grep \"The C compiler identification\""`
 
-  sox -D -twav SDRSharp_20160101_231914Z_12kHz_IQ.wav -traw -eunsigned-int -b8 -r512k - | tee -i uint8.dat | build/demodulator -i - -o - -r1 | sox -traw -b32 -ef -r128k - -traw -es -b16 -r48k - | dsd -i - -o /dev/null -n -w out.wav
-  sox -D -twav SDRSharp_20160101_231914Z_12kHz_IQ.wav -traw -es -b16 -r512k - | tee -i int16.dat | build/demodulator -i - -o - | sox -traw -b32 -ef -r128k - -traw -es -b16 -r48k - | dsd -i - -o /dev/null -n -w out16.wav
+  printRunInfo "$key" "$compiler"
+  sox -q -D -twav SDRSharp_20160101_231914Z_12kHz_IQ.wav -traw -eunsigned-int -b8 -r512k - 2>/dev/null | tee -i uint8.dat | build/demodulator -i - -o - -r1 | sox -traw -b32 -ef -r${arr[$key]} - -traw -es -b16 -r48k - | dsd -i - ${audioOutOpts} #>/dev/null 2>&1
 
-  time build/demodulator -i uint8.dat -o file -r1 && rm file
-  time build/demodulator -i uint8.dat -o file -r1 && rm file
+  printRunInfo "$key" "$compiler"
+  sox -q -D -twav SDRSharp_20160101_231914Z_12kHz_IQ.wav -traw -es -b16 -r512k - 2>/dev/null| tee -i int16.dat | build/demodulator -i - -o - | sox -traw -b32 -ef -r${arr[$key]} - -traw -es -b16 -r48k - | dsd -i - ${audioOutOpts} #>/dev/null 2>&1
+
+  echo ""
+  echo ":: Timing uint8"
+  printRunInfo "$key" "$compiler"
   time build/demodulator -i uint8.dat -o file -r1
-  sox -traw -b32 -ef -r128k file -traw -es -b16 -r48k - | dsd -i - -o /dev/null -n && rm file uint8.dat
-  time build/demodulator -i int16.dat -o file && rm file
-  time build/demodulator -i int16.dat -o file && rm file
+  rm file
+  time build/demodulator -i uint8.dat -o file -r1
+  rm file
+  time build/demodulator -i uint8.dat -o file -r1
+  #sox -traw -b32 -ef -r${arr[$key]} file -traw -es -b16 -r48k - | dsd -q -i - -o /dev/null -n && rm -f file uint8.dat
+  echo ":: End Timing uint8"
+  echo ""
+  echo ":: Timing int16"
+  printRunInfo "$key" "$compiler"
   time build/demodulator -i int16.dat -o file
-  sox -traw -b32 -ef -r128k file -traw -es -b16 -r48k - | dsd -i - -o /dev/null -n && rm file int16.dat
+  rm file
+  time build/demodulator -i int16.dat -o file
+  rm file
+  time build/demodulator -i int16.dat -o file
+  #sox -traw -b32 -ef -r${arr[$key]} file -traw -es -b16 -r48k - | dsd -q -i - -o /dev/null -n && rm -f file int16.dat
+  echo ":: End Timing int16"
+  rm -rf file int16.dat uint8.dat ||:
 done
+
