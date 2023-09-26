bce713b8cb88ab6def535dc3b5bdf4c442112384
Patrick Eads <peads@users.noreply.github.com>
2023-09-26 13:59:02 +0000

    incremental

--- cuda/cuc_matrix.cu
+++ cuda/cuc_matrix.cu
@@ -21,27 +21,35 @@
 #include "nvidia.cuh"
 
 __global__
-void fmDemod(const uint8_t *buf, const uint32_t len, float *result) {
+void fmDemod(uint8_t *idata, const uint32_t len, const float squelchLevel, float *result) {
 
     uint32_t i;
     uint32_t index = blockIdx.x * blockDim.x + threadIdx.x;
     uint32_t step = blockDim.x * gridDim.x;
-    cuComplex a, b, z;
+    cuComplex z;
 
-    for (i = index; i < len; i += step) {
+    for (i = index; i < len; i += step + 8) {
 
-        a = {
-            __int2float_rn(buf[i] + buf[i + 2] - 254),
-            __int2float_rn(254 - buf[i + 1] - buf[i + 3])};
-        b = {
-            __int2float_rn(buf[i + 4] + buf[i + 6] - 254),
-            __int2float_rn(buf[i + 5] + buf[i + 7] - 254)};
-        z = cuCmulf(a, b);
+        // power = (I^2 + Q^2) / 2 / (50 Ohms) = (I^2 + Q^2) / (100 Ohms)
+        // power = (I^2 + Q^2) / 2 / (50 Ohms) = (I^2 + Q^2) / (100 Ohms)
+        if (squelchLevel != 0.f && (float)(
+                (idata[i] * idata[i]) + (idata[i + 1] * idata[i + 1]) +
+                (idata[i + 2] * idata[i + 2]) + (idata[i + 3] * idata[i + 3]) +
+                (idata[i + 4] * idata[i + 4]) + (idata[i + 5] * idata[i + 5]) +
+                (idata[i + 6] * idata[i + 4]) + (idata[i + 7] * idata[i + 7]))
+            * 0.01f < squelchLevel) {
+            result[i >> 2] = 0.f;
+        } else {
 
-        z.y = __fmul_rn(64.f, z.y);
-        z.x = __fmul_rn(z.y, __frcp_rn(__fmaf_rn(23.f, z.x, 41.f)));
-        result[i >> 2] = isnan(z.x) ? 0.f : z.x; // delay line
+            z = cuCmulf(
+                {(float)(idata[i] + idata[i + 2] - 254), (float)(254 - idata[i + 1] - idata[i + 3])},
+                {(float)(idata[i + 4] + idata[i + 6] - 254), (float)(idata[i + 5] + idata[i + 7] - 254)});
+
+            z.y = __fmul_rn(64.f, z.y);
+            z.x = __fmul_rn(z.y, __frcp_rn(__fmaf_rn(23.f, z.x, 41.f)));
+            result[i >> 2] = isnan(z.x) ? 0.f : z.x; // delay line
 //        result[i >> 2] = atan2f(z.y, z.x);
+        }
     }
 }
 
@@ -56,13 +64,13 @@ extern "C" int processMatrix(FILE *__restrict__ inFile,
     float *dResult;
     float *hResult;
     size_t readBytes;
-    const uint8_t isGain = fabsf(1.f - gain) > GAIN_THRESHOLD;
+    float squelchLevel = 750.f;
+//    const uint8_t isGain = fabsf(1.f - gain) > GAIN_THRESHOLD;
 
-
-    cudaMallocHost(&hBuf, sizeof(cuComplex) * (DEFAULT_BUF_SIZE >> 1));
-    cudaMalloc(&dBuf, sizeof(cuComplex) * (DEFAULT_BUF_SIZE >> 1));
-    cudaMallocHost(&hResult, (DEFAULT_BUF_SIZE >> 2) * OUTPUT_ELEMENT_BYTES);
-    cudaMalloc(&dResult, (DEFAULT_BUF_SIZE >> 2) * OUTPUT_ELEMENT_BYTES);
+    cudaMalloc(&dBuf, DEFAULT_BUF_SIZE);
+    cudaMalloc(&dResult, (DEFAULT_BUF_SIZE >> 2) * sizeof(float));
+    cudaMallocHost(&hBuf, DEFAULT_BUF_SIZE);
+    cudaMallocHost(&hResult, (DEFAULT_BUF_SIZE >> 2) * sizeof(float));
 
     hBuf[0] = 0;
     hBuf[1] = 0;
@@ -79,15 +87,14 @@ extern "C" int processMatrix(FILE *__restrict__ inFile,
         }
 
         cudaMemcpy(dBuf, hBuf, DEFAULT_BUF_SIZE, cudaMemcpyHostToDevice);
-
-        fmDemod<<<GRIDDIM, BLOCKDIM>>>(dBuf, readBytes + 2, dResult);
-
+        cudaDeviceSynchronize();
+        fmDemod<<<GRIDDIM, BLOCKDIM>>>(dBuf, DEFAULT_BUF_SIZE, squelchLevel, dResult);
         cudaMemcpy(hResult,
             dResult,
-            ((readBytes + 2) >> 2) * OUTPUT_ELEMENT_BYTES,
+            (DEFAULT_BUF_SIZE >> 2) * sizeof(float),
             cudaMemcpyDeviceToHost);
 
-        fwrite(hResult, OUTPUT_ELEMENT_BYTES, (readBytes + 2) >> 2, (FILE *) outFile);
+        fwrite(hResult, sizeof(float), (readBytes + 2) >> 2, (FILE *) outFile);
     }
 
     cudaFreeHost(hBuf);
