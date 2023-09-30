6fe13fa151674a1f3ea25ac2086040ff8841b00b
Patrick Eads <peads@users.noreply.github.com>
2023-09-30 16:35:11 -0400

    headers and formatting

--- plot.py
+++ plot.py
@@ -10,7 +10,7 @@
 # the Free Software Foundation, version 3.
 #
 # This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY without even the implied warranty of
+# WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 #
