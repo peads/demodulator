a1001e1ae0c2498d71a67e5c965335ef7650bdd8
Patrick Eads <peads@users.noreply.github.com>
2023-10-06 01:52:18 +0000

    incermantl

---
+++ intrinsics/neon/matrix.c
@@ -0,0 +1,32 @@
+/*
+ * This file is part of the demodulator distribution
+ * (https://github.com/peads/demodulator).
+ * with code originally part of the misc_snippets distribution
+ * (https://github.com/peads/misc_snippets).
+ * Copyright (c) 2023 Patrick Eads.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <arm_neon.h>
+#include "definitions.h"
+#include "matrix.h"
+
+void *processMatrix(void *ctx) {
+
+    return NULL;
+}
+
+void allocateBuffer(void **buf, const size_t len) {
+
+//    *buf = calloc(len, 1);
+}
