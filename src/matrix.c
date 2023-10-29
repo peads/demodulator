b7ca1cbb1d22379ceacca4ead4447cb6ba91fdc5
Patrick Eads <peads@users.noreply.github.com>
2023-10-29 20:23:10 +0000

    incre

--- src/matrix.c
+++ src/matrix.c
@@ -22,31 +22,93 @@
 #include "matrix.h"
 #include "fmath.h"
 
-static inline void fmDemod(const void *__restrict__ in,
-                           const uint32_t len,
+static inline void fmDemod(const float *__restrict__ buf,
+                           const size_t len,
                            const float gain,
                            float *__restrict__ result) {
-
-    float temp[4] = {0.f, 0.f, 0.f, 0.f};
-    const uint8_t *buf = in;
-    uint32_t i;
+    size_t i;
     float zr, zj, ac, bd;
 
     for (i = 0; i < len; i += 4) {
 
-        temp[0] = 0.25f * (float) (buf[i] + buf[i + 2] + buf[i + 4] + buf[i + 6] - 508);       // ar
-        temp[1] = 0.25f * (float) (buf[i + 1] + buf[i + 3] + buf[i + 5] + buf[i + 7] - 508);   // aj
-        temp[2] = 0.25f * (float) (buf[i + 8] + buf[i + 10] + buf[i + 12] + buf[i + 14] - 508);   // br
-        temp[3] = 0.25f * (float) (508 - buf[i + 9] - buf[i + 11] - buf[i + 13] - buf[i + 15]);   // -bj
-
-        ac = temp[0] * temp[2];
-        bd = temp[1] * temp[3];
+        ac = buf[0] * buf[2];
+        bd = buf[1] * buf[3];
         zr = ac - bd;
-        zj = (temp[0] + temp[1]) * (temp[2] + temp[3]) - (ac + bd);
-        result[i >> 3] = atan2f(zj, zr);
+        zj = (buf[0] + buf[1]) * (buf[2] + buf[3]) - (ac + bd);
+        result[i>>2] = atan2f(zj, zr);
 //        zr = 64.f * zj * frcpf(23.f * zr + 41.f * hypotf(zr, zj));
 //
 //        result[i >> 3] = isnan(zr) ? 0.f : gain ? zr * gain : zr;
+    }
+}
+
+void shiftOrigin(void *__restrict__ in, size_t len) {
+    size_t i;
+    int8_t *buf = in;
+    for (i = 0; i < len; i += 8){
+        buf[i] -= 127;
+        buf[i+1] -= 127;
+        buf[i+2] -= 127;
+        buf[i+3] -= 127;
+        buf[i+4] -= 127;
+        buf[i+5] -= 127;
+        buf[i+6] -= 127;
+        buf[i+7] -= 127;
+    }
+
+//    temp[0] = 0.25f * (float) (buf[i] + buf[i + 2] + buf[i + 4] + buf[i + 6] - 508);       // ar
+//    temp[1] = 0.25f * (float) (buf[i + 1] + buf[i + 3] + buf[i + 5] + buf[i + 7] - 508);   // aj
+//    temp[2] = 0.25f * (float) (buf[i + 8] + buf[i + 10] + buf[i + 12] + buf[i + 14] - 508);   // br
+//    temp[3] = 0.25f * (float) (508 - buf[i + 9] - buf[i + 11] - buf[i + 13] - buf[i + 15]);   // -bj
+}
+
+void filterHighpass(void *__restrict__ in, size_t len, float *__restrict__ out) {
+    static const float BW_CONSTS[16][2] = {
+           {-0.0980171f, 0.995185f},
+           {-0.290285f, 0.95694f},
+           {-0.471397f,  0.881921f},
+           {-0.634393f,  0.77301f},
+           {-0.77301f,   0.634393f},
+           {-0.881921f,  0.471397f},
+           {-0.95694f,   0.290285f},
+           {-0.995185f,  0.0980171f},
+           {-0.995185f,  -0.0980171f},
+           {-0.95694f,   -0.290285f},
+           {-0.881921f,  -0.471397f},
+           {-0.77301f,   -0.634393f},
+           {-0.634393f,-0.77301f},
+           {-0.471397f,-0.881921f},
+           {-0.290285f,-0.95694f},
+           {-0.0980171f,-0.995185f}};
+    static const float Wc = 1.f;
+    size_t i,j;
+    float accR, accJ, currR, currJ, normSquared;
+    const float *constPtr;
+    int8_t *buf = in;
+    for (i = 0; i < len; i+=2) {
+        accR = 1.f;
+        accJ = 1.f;
+        currR = Wc/((float) buf[i]);
+        currJ = Wc/((float) buf[i+1]);
+        for (j = 0; j < 16; ++j) {
+            constPtr = BW_CONSTS[j];
+            accR *= currR - constPtr[0];
+            accJ *= currJ - constPtr[1];
+        }
+        normSquared = accR * accR + accJ * accJ;
+        out[i] = accR/normSquared * (float) buf[i];
+        out[i+1] = accJ/normSquared * (float) buf[i+1];
+    }
+}
+
+void crudeLowpass(void *__restrict__ in, size_t len) {
+
+    size_t i, j;
+    int8_t *buf = in;
+    for (i = 0; i < len; i+=4) {
+        j = i >> 1;
+        buf[j] = (int8_t)(buf[i] + buf[i+2]);
+        buf[j+1] = (int8_t)(buf[i+1] + buf[i+3]);
     }
 }
 
@@ -54,7 +116,8 @@ void *processMatrix(void *ctx) {
 
     consumerArgs *args = ctx;
     void *buf = calloc(DEFAULT_BUF_SIZE, 1);
-    float *result = calloc(DEFAULT_BUF_SIZE >> 3, sizeof(float));
+    float *fBuf = calloc(DEFAULT_BUF_SIZE, sizeof(float));
+    float *result = calloc(DEFAULT_BUF_SIZE>>2, sizeof(float));
 
     while (!args->exitFlag) {
 
@@ -64,7 +127,10 @@ void *processMatrix(void *ctx) {
         pthread_mutex_unlock(&args->mutex);
         sem_post(&args->empty);
 
-        fmDemod(buf, DEFAULT_BUF_SIZE, args->gain, result);
+        shiftOrigin(buf, DEFAULT_BUF_SIZE);
+        crudeLowpass(buf, DEFAULT_BUF_SIZE);
+//        filterHighpass(buf, DEFAULT_BUF_SIZE, fBuf);
+        fmDemod(fBuf, DEFAULT_BUF_SIZE >> 1, args->gain, result);
         fwrite(result, sizeof(float), DEFAULT_BUF_SIZE >> 3, args->outFile);
     }
     free(buf);
