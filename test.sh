dba869995d175e5ff254f0b622485453abf7c376
Patrick Eads <peads@users.noreply.github.com>
2023-11-19 07:36:32 +0000

    Fixed off by one bug while I was adding filter commandline options

--- test.sh
+++ test.sh
@@ -72,21 +72,6 @@ function executeRun2() {
     | multimon-ng -q -c -aFLEX_NEXT -
 }
 
-function join_by() {
-  local IFS="$1"; shift; echo "$*";
-}
-
-function findSmVersion() {
-  nvc=`which nvcc`
-  var=(`echo ${nvc} | sed 's/\// /g'`)
-  unset var[-1]
-  unset var[-1]
-  var="/`join_by "/" ${var[*]}`/extras/demo_suite/deviceQuery"
-  var=`eval $var | grep "CUDA Capability Major/Minor version number" \
-    | sed -e "s/\s\+CUDA Capability Major\/Minor version number:\s\+//g"`
-  echo $var
-}
-
 findCompiler gcc hasGcc
 findCompiler clang hasClang
 findCompiler icc hasIcc
@@ -122,9 +107,9 @@ for compiler in ${compilers[@]}; do
   executeRun "-L12500"
 
   echo ":: STARTING TIMED RUNS 1 FOR: ${compiler} -DNO_INTRINSICS=ON dsd with lowpass in"
-  executeTimedRun "-L12500"
-  executeTimedRun "-L12500"
-  executeTimedRun "-L12500"
+  executeTimedRun "-L12500 -D21"
+  executeTimedRun "-L12500 -D21"
+  executeTimedRun "-L12500 -D21"
   echo ":: COMPLETED TIMED RUNS 1 FOR: ${compiler} -DNO_INTRINSICS=ON dsd with lowpass in"
   rm -rf file uint8.dat
 
